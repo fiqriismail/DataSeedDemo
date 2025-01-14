c540e77c302f57a94e876079f74129b5d21d3480
Fiqri Ismail <fiqri.ismail@gmail.com>
2025-01-14 16:56:40 +0530

    Project Start

--- DataSeedDemo/obj/Debug/net9.0/DataSeedDemo.AssemblyInfo.cs
+++ DataSeedDemo/obj/Debug/net9.0/DataSeedDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataSeedDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+412f9a3b1fdd331800156628ce6286c271a14f0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataSeedDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataSeedDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
